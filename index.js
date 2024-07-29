e5cc6c5234ddf86acd9ef0736255a87bea31803a
Sare@20 <umapnishad1@gmail.com>
2024-07-29 11:50:04 +0530

    Added Functionality of Image and docs file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const nodemailer = require ('nodemailer');
+const path = require('path');
+const fs = require('fs');
 
 let transportmail = nodemailer.createTransport({
     service:'gmail',
@@ -7,13 +9,26 @@ let transportmail = nodemailer.createTransport({
         pass:'vyapzjvywepukzph'
     }
 })
+// twilio
 let mailContent={
     from :'umapnishad1@gmail.com',
     to :'umapnishad2510@gmail.com',
     subject:'Verification Mail',
-    text: 'This mail is regarding to email verification'
+    html: '<p>This mail is regarding email verification.</p><img src="cid:image@unique.cid"/>',
+    attachments: [
+        {
+            filename: 'document.docx',
+            path: path.join(__dirname, './myinfo.txt' )
+        },
+        {
+            filename: 'image.jpg',
+            path: path.join(__dirname, './email_img.png'),
+            cid: 'image@unique.cid'
+        }
+    ]
 }
-
+// './myinfo.txt' 
+// './email_img.png'
 transportmail.sendMail(mailContent,function(err,val){
     if(err){
         console.log(err);
